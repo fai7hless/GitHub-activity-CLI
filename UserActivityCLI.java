049a7d759d1de0b5417a52a767bf97641df7bf99
Vasil Popov <vasipopov@tu-sofia.bg>
2025-02-16 18:31:31 +0200

    Added a displayUserData method to process the user data and display it in a formatted way. Used gson library to parse the json data.

--- UserActivityCLI.java
+++ UserActivityCLI.java
@@ -3,6 +3,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class UserActivityCLI {
     public static void main(String[] args) {
@@ -24,7 +28,8 @@ public class UserActivityCLI {
                 System.out.println("User not found");
             }
             if (response.statusCode() == 200) {
-                System.out.println(response.body());
+                JsonArray arr = JsonParser.parseString(response.body()).getAsJsonArray();
+                displayUserData(arr);
             }else{
                 System.out.println("Error: " + response.statusCode());
             }
@@ -34,5 +39,53 @@ public class UserActivityCLI {
         }
     }
 
+
+    private static void displayUserData(JsonArray arr){
+        for (JsonElement element : arr){
+            JsonObject obj = element.getAsJsonObject();
+            String type = obj.get("type").getAsString();
+            String msg;
+            switch (type) {
+                case "PushEvent":
+                    int count = obj.get("payload").getAsJsonObject().get("size").getAsInt();
+                    msg = "Pushed " + count + " commit/s to " + obj.get("repo").getAsJsonObject().get("name");
+                    break;
+
+                case "CreateEvent":
+                    msg = "Created " + obj.get("payload").getAsJsonObject().get("ref_type").getAsString()
+                    + " in " + obj.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+                
+                case "WatchEvent":
+                    msg = "Starred " + obj.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+
+                case "IssuesEvent":
+                    msg = obj.get("payload").getAsJsonObject().get("action").getAsString()
+                    + " issue in " + obj.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+                
+                case "ForkEvent":
+                    msg = "Forked " + obj.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;  
+
+                case "DeleteEvent":
+                    msg = "Deleted " + obj.get("payload").getAsJsonObject().get("ref_type").getAsString()
+                    + " in " + obj.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+                
+                case "PullRequestEvent":
+                    msg = obj.get("payload").getAsJsonObject().get("action").getAsString()
+                    + " pull request in " + obj.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+                    
+                default:
+                    msg = obj.get("type").getAsString().replace("Event", "")
+                            + " in " + obj.get("repo").getAsJsonObject().get("name").getAsString();
+                    break;
+            }
+            System.out.println(msg);
+        }
+    }
     
 }
